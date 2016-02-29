d69736376cfbfa08a9e15562f50db5e518c338db
jhoss <hoss@phys.ethz.ch>
2016-02-29 11:08:45 +0100

    SUSY3L: small additions to plotting scripts

--- scripts/plotting3l.sh
+++ scripts/plotting3l.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #VAR=("njets" "nbjets" "met" "ht" "pt1" "pt2" "pt3" "srs")
-VAR=("njets" "nbjets" "met" "ht" "srs")
+VAR=("ttbar_lep1_dxy" "ttbar_lep1_dz" "ttbar_lep2_dxy" "ttbar_lep2_dz" "ZMuMu_lep1_dxy" "ZMuMu_lep1_dz" "ZMuMu_lep2_dxy" "ZMuMu_lep2_dz" "ZElEl_lep1_dxy" "ZElEl_lep1_dz" "ZElEl_lep2_dxy" "ZElEl_lep2_dz" "Wl_lep1_dxy" "Wl_lep1_dz" "Wl_lep2_dxy" "Wl_lep2_dz" "Zl_lep1_dxy" "Zl_lep1_dz" "Zl_lep2_dxy" "Zl_lep2_dz" )
 #VAR=("ftype")
 #VAR=("srs")
 
@@ -11,7 +11,8 @@ for var in ${VAR[@]}; do
     variable=$var
 
     cp cards/susy3l.C cards/tmpFiles/susy3l.C
-    sed -i 's|VARIABLE|'$variable'|' cards/tmpFiles/susy3l.C
+    #sed -i 's|VARIABLE|'$variable'|' cards/tmpFiles/susy3l.C
+    sed -i 's|OBS|'$variable'|' cards/tmpFiles/susy3l.C
 
     root -l -b cards/tmpFiles/susy3l.C
 
