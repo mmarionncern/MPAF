f43b554fdd4a11fa8c8052b2b96ec4dfc8a644a8
jhoss <hoss@phys.ethz.ch>
2016-05-04 09:36:10 +0200

    SUSY3L: new scripts for T5ttttdeg sample (2)

---
+++ scripts/script_scan/step8/retrieveLimsFromScanT5ttttdeg.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo -n "" > Limits_T5ttttdeg_scan.txt
+
+mydir=/mnt/t3nfs01/data01/shome/jhoss/logs
+
+for i in `ls $mydir/*.log`; do
+
+spl=`echo $i | tr '_' ' '`
+
+m1=`echo $spl | awk '{print $5}'`
+m1=${m1#m}
+m2=`echo $spl | awk '{print $6}'`
+m2=${m2%.txt.log}
+m2=${m2#m}
+
+#echo $m1
+#echo $m2
+
+obs=`grep "Observed"    $i | awk '{print $5}'`
+exp=`grep "Expected 50" $i | awk '{print $5}'`
+d1s=`grep "Expected 16" $i | awk '{print $5}'`
+u1s=`grep "Expected 84" $i | awk '{print $5}'`
+u2s=`grep "Expected 97" $i | awk '{print $5}'`
+d2s=`grep "Expected  2" $i | awk '{print $5}'`
+
+if [[ -n "$exp" ]]; then
+    echo -e $m1"\t"$m2"\t"$obs"\t"$exp"\t"$d1s"\t"$u1s"\t"$d2s"\t"$u2s >> Limits_T5ttttdeg_scan.txt
+fi
+
+
+
+done
